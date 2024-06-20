354047950ce18059891db9cb0db8191111236bc7
HugoDF <HugoDF@users.noreply.github.com>
2024-06-20 21:04:27 +0100

    be Node.js < 20.15.0 compatible

--- server.test.js
+++ server.test.js
@@ -1,8 +1,9 @@
 import { test } from "node:test";
+import assert from "assert/strict";
 import supertest from "supertest";
 import app from "./server.js";
 
 test("/ returns fixed data", async (t) => {
 	const res = await supertest(app).get("/");
-	t.assert.deepEqual(res.body, { test: "data" });
+	assert.deepEqual(res.body, { test: "data" });
 });
